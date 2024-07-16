c2eabab3cb9430ee27d39f9ab9c65f81de57404a
b9cv9 <emil.kurakin95@mail.ru>
2024-07-17 00:02:21 +0400

    v2.6

--- univercity game project collab/Assets/scripts/difficulty setter.cs
+++ univercity game project collab/Assets/scripts/difficulty setter.cs
@@ -1,11 +1,26 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class difficultysetter : MonoBehaviour
 {
     [SerializeField] private int difficulty;
+    [SerializeField] private Sprite active, notActive;
 
     public void setDifficulty()
     {
         PlayerPrefs.SetInt("diff", difficulty);
+    }
+
+    private void Update()
+    {
+        if (difficulty == PlayerPrefs.GetInt("diff", 1))
+        {
+            this.GetComponent<Image>().sprite = active;
+        }
+        else
+        {
+            this.GetComponent<Image>().sprite = notActive;
+        }
     }
 }
