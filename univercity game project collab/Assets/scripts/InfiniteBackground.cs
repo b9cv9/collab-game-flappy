8887e97e0ca1de9c5edac0357d287436453a38f8
b9cv9 <emil.kurakin95@mail.ru>
2024-07-17 00:54:52 +0400

    background update

---
+++ univercity game project collab/Assets/scripts/InfiniteBackground.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class InfiniteBackground : MonoBehaviour
+{
+    public GameObject background1;
+    public GameObject background2;
+    public float backgroundSpeed = 0.1f;
+    [SerializeField] private float backgroundWidth;
+    void Update()
+    {
+        MoveBackground(background1);
+        MoveBackground(background2);
+
+        if (background1.transform.position.x <= -backgroundWidth/2)
+        {
+            RepositionBackground(background1, background2);
+        }
+        if (background2.transform.position.x <= -backgroundWidth/2)
+        {
+            RepositionBackground(background2, background1);
+        }
+    }
+
+    void MoveBackground(GameObject background)
+    {
+        background.transform.Translate(Vector3.left * backgroundSpeed * Time.deltaTime);
+    }
+
+    void RepositionBackground(GameObject background, GameObject otherBackground)
+    {
+        background.transform.position = new Vector3(otherBackground.transform.position.x + backgroundWidth, background.transform.position.y, background.transform.position.z);
+    }
+}
